2103876c246ee8318c0f225f6d7909aab164071f
Alessa Messineo <apmessineo@gmail.com>
2017-03-15 11:05:30 -0400

    update routes for bar search feature, fill out Restaurant.update in models/restaurant.js

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,24 +3,28 @@ const db = require('../config/database.js');
 let Restaurant = {};
 
 Restaurant.findAllById = (user_id) => {
-  // return db.manyOrNone(`SELECT * FROM favorites where user_id = $1`, [user_id])
+  return db.manyOrNone(`SELECT * FROM favorites where user_id = $1`, [user_id])
 }
 
 
 Restaurant.create = (restaurant, user_id) => {
-  // something like, return db.none
-  // INSERT INTO favorites (user_id, name, location, rating, etc)
-  // VALUES ($1, $2, $3, $4),
-  // [id, restaurant.name, restaurant.location, restaurant.rating]
+return db.none(`INSERT INTO favorites (user_id, name, html_attributions, price_level,
+   formatted_address, review_count, rating, comment)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+  [user_id, restaurant.name, restaurant.html_attributions, restaurant.price_level, restaurant.formatted_address,
+  restaurant.review_count, restaurant.rating, null])
 
 }
 
 Restaurant.update = (restaurant, user_id) => {
-
+  return db.none(`UPDATE favorites SET user_id = $1, name = $2, html_attributions = $3,
+    price_level = $4, formatted_address = $5, review_count = $6, rating = $7, comment = $8`,
+    [user_id, restaurant.name, restaurant.html_attributions, restaurant.price_level, restaurant.formatted_address,
+  restaurant.review_count, restaurant.rating, restaurant.comment])
 }
 
 Restaurant.destroy = (restaurant_id) => {
-//   return db.none(`DELETE FROM favorites where id = $1`, [restaurant_id])
+  return db.none(`DELETE FROM favorites where id = $1`, [restaurant_id])
 };
 
 module.exports = Restaurant;
--- services/restaurants.js
+++ services/restaurants.js
@@ -3,9 +3,10 @@ const fetch = require('node-fetch');
 const googlePlaces = {};
 const apiKey = process.env.API_KEY;
 
-googlePlaces.search = (zip) => {
-  console.log('zip is', zip)
-  return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${zip}&key=${apiKey}&type=restaurant&opennow=true`
+googlePlaces.search = (zip, category) => {
+  console.log('zip is', zip);
+  console.log('category is: ', category)
+  return fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${zip}&key=${apiKey}&type=${category}&opennow=true`
 
   );
 }
